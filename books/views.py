b3bd1cce89e4f373a985c39d81a3ff1679c2b5e5
b1017206 <b1017206@fun.ac.jp>
2021-01-17 17:56:03 +0900

    no message

---
+++ books/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+def index_template(request):
+    myapp_data = {
+    'app': 'Django',
+    'is_weekday':True
+    }
+    return render(request, 'index.html', myapp_data)
