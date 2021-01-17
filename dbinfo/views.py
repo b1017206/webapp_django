5df04169db3a68b991bc92cbf3c5b3abdfc33163
松尾龍磨 <>
2021-01-18 01:46:58 +0900

    no message

--- dbinfo/views.py
+++ dbinfo/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 def index_template(request):
+    #products = DBinfo.objects.filter(id=1)
     myapp_data = {
     'app': 'Django',
-    'is_weekday':True
+    'is_weekday':True,
+    #'id':products,
     }
-    return render(request, 'index.html', myapp_data)
+    return render(request,'index.html', myapp_data)
